1eb8c2db1d1149e03054b2a4651294ac81aa164d
khyathimsit <khyathi17@msitprogram.net>
2018-08-01 20:37:50 +0530

    ninth commit

---
+++ cspp1-practice/Module3/sum_1_end1.py
@@ -0,0 +1,14 @@
+'''
+Author: Khyathi Cheedalla
+
+Date: 1-8-2018
+
+Encoding: UTF-8
+
+'''
+NUM = int(input("Enter the number"))
+I = 1
+SUM = 0
+for I in range(I, NUM+1, 1):
+    SUM = SUM+I
+print(SUM)
