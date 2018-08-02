6ceaa19f450611650e04616c9b05fc8a69a46d75
khyathimsit <khyathi17@msitprogram.net>
2018-08-02 18:31:43 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-practice/p2/bob_counter.py
+++ cspp1-practice/p2/bob_counter.py
@@ -7,7 +7,7 @@ def main():
     ''' This code prints the number of times that bob occured in the given string'''
     s_1 = input()
     co_unt = 0
-    for i in range(0,len(s_1)-2):
+    for i in range(0, len(s_1)-2):
         if s_1[i] == 'b' and s_1[i+1] == 'o' and s_1[i+2] == 'b':
             co_unt += 1
     print(co_unt)
