7f11cda812ace11a7ebb0e5c6c884d39829d079f
khyathimsit <khyathi17@msitprogram.net>
2018-08-02 19:24:38 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-practice/p3/longest_substring.py
+++ cspp1-practice/p3/longest_substring.py
@@ -8,24 +8,24 @@ Date: 2-8-18
 def main():
     ''' This code is used to find the longest substring in the given string'''
     s_1 = input()
-    s1 = ''
-    fs = ''
-    n = len(s_1)
+    s_2 = ''
+    f_s = ''
+    n_1 = len(s_1)
     f_max = 0
     f_cnt = 0
-    fs = s_1[0]
-    for i in range(0, n-1):
+    f_s = s_1[0]
+    for i in range(0, n_1-1):
         if ord(s_1[i]) <= ord(s_1[i+1]):
-        	f_cnt += 1
-        	fs = fs + s_1[i+1]
+            f_cnt += 1
+            f_s = f_s + s_1[i+1]
         else:
-        	if f_cnt > f_max:
-        	    f_max = f_cnt
-        	    s1 = fs
-        	f_cnt = 0
-        	fs = s_1[i+1]
+            if f_cnt > f_max:
+                f_max = f_cnt
+                s_2 = f_s
+            f_cnt = 0
+            f_s = s_1[i+1]
     if f_cnt > f_max:
-        s1 = fs
-    print(s1)
+        s_2 = f_s
+    print(s_2)
 if __name__ == "__main__":
-	main()
+    main()
