68dc9331f0874846f120024fa0631113317cd2dd
khyathimsit <khyathi17@msitprogram.net>
2018-08-02 17:23:09 +0530

    " -> 0 of 4 passed. pylint: 8.89/10 "

--- cspp1-practice/p1/vowels_counter.py
+++ cspp1-practice/p1/vowels_counter.py
@@ -6,13 +6,14 @@ Date: 2-8-16
 '''
 def main():
     '''
-       This function is used to find the vowels in the given string
+        This function is used to find the vowels in the given string
     '''
     st_r = input()
     co_unt = 0
     for cha_r in st_r:
         if cha_r in ('a', 'e', 'i', 'o', 'u'):
             co_unt += 1
+    
     print(co_unt)
 
 if __name__ == "main":
