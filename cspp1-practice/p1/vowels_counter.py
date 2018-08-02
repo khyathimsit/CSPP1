81badaa556a791eb69ea3ede0ab6aa90e88abcf4
khyathimsit <khyathi17@msitprogram.net>
2018-08-02 16:49:14 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- cspp1-practice/p1/vowels_counter.py
+++ cspp1-practice/p1/vowels_counter.py
@@ -7,7 +7,7 @@ Date: 2-8-16
 def main():
 	 ST_R = input()
 	 CO_UNT = 0
-	 for ch_ar in ST_R
+	 for ch_ar in ST_R:
          if CH_AR == 'a' or CH_AR == 'e' or CH_AR == 'i' or CH_AR == 'o' or CH_AR == 'u':
              CO_UNT += 1
      print(CO_UNT)
