a108e11b3d53e1c18371874077c4c55fe9f6dd27
khyathimsit <khyathi17@msitprogram.net>
2018-08-03 16:23:39 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

---
+++ cspp1-practice/m5/p1/perfect_cube.py
@@ -0,0 +1,17 @@
+'''
+Author: khyathi
+'''
+
+def main():
+    '''to find whether the given number is perfect cube or not'''
+    x_1 = int(input('Enter an integer: '))
+    ans = 0
+    while ans**3 < x_1:
+        ans = ans + 1
+    if ans**3 != x_1:
+        print(str(x_1) + ' is not a perfect cube')
+    else:
+        print('Cube root of ' + str(x_1) + ' is ' + str(ans))
+
+if __name__ == "__main__":
+    main()
