3cc189e33093fad7083b9f9f3f6bdf3ae95a0840
khyathimsit <khyathi17@msitprogram.net>
2018-08-04 12:54:52 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- cspp1-practice/m6/p3/digit_product.py
+++ cspp1-practice/m6/p3/digit_product.py
@@ -7,12 +7,12 @@ def main():
     '''
     n = int(input())
     p = 1
-    if n == 0:
-        print (0)
     while i>0
         a = n % 10
         p = p * a
         y = n // 10
     print(p)
+    if n == 0:
+        print (0)
 if __name__ == "__main__":
     main()
