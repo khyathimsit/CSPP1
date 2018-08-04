49826c4ebeacdd7184449e3944676ab63bc4271a
khyathimsit <khyathi17@msitprogram.net>
2018-08-04 12:08:21 +0530

    " -> 1 of 7 passed. pylint: 2.50/10 "

---
+++ cspp1-practice/m6/p2/special_char.py
@@ -0,0 +1,16 @@
+'''
+Author : Khyathi
+Date: 4-8-18
+Exam 1
+'''
+def main():
+    '''
+    This code is used to replace all the special characters with a space
+    '''
+    str_input = input()
+    for i in range(0, len(str_input)):
+        if char in ('!', '@', '#', '$', '%', '^', '*'):
+            str_input.append(" ")
+        print(str_input)
+if __name__ == "__main__":
+    main()
