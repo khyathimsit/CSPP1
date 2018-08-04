0229322e0b57dc44fa81dd29f600f7f4b458b816
khyathimsit <khyathi17@msitprogram.net>
2018-08-04 13:03:16 +0530

    " -> 4 of 7 passed. pylint: 5.83/10 "

--- cspp1-practice/m6/p3/digit_product.py
+++ cspp1-practice/m6/p3/digit_product.py
@@ -7,12 +7,12 @@ def main():
     '''
     n = int(input())
     p = 1
+    if n == 0:
+        print("0")
     while n > 0:
         a = n % 10
         n = n // 10
         p = p * a
     print(p)
-    #if n == 0:
-        #print("0")
 if __name__ == "__main__":
     main()
