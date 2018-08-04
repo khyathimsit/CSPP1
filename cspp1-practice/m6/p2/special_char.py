62080ec4ef4f93f5c61726eef25b06ba4533e17e
khyathimsit <khyathi17@msitprogram.net>
2018-08-04 12:18:23 +0530

    " -> 1 of 7 passed. pylint: 8.89/10 "

--- cspp1-practice/m6/p2/special_char.py
+++ cspp1-practice/m6/p2/special_char.py
@@ -8,9 +8,10 @@ def main():
     This code is used to replace all the special characters with a space
     '''
     str_input = input()
+    str_2 = ""
     for char in str_input:
-        if char in('!@#$%^*'):
+        if char in "!@#$%^*":
             str_input.append(" ")
-        print(str_input)
+    print(str_input)
 if __name__ == "__main__":
     main()
