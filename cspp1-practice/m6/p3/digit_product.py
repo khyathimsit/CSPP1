46f4dc0fbd7c62e7e1b26c9a148d62e67d96381b
khyathimsit <khyathi17@msitprogram.net>
2018-08-04 13:04:27 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- cspp1-practice/m6/p3/digit_product.py
+++ cspp1-practice/m6/p3/digit_product.py
@@ -5,14 +5,14 @@ def main():
     '''
     This code is to print the digit product
     '''
-    n = int(input())
-    p = 1
-    if n == 0:
+    n_input = int(input())
+    p_out = 1
+    if n_input == 0:
         print("0")
-    while n > 0:
-        a = n % 10
-        n = n // 10
-        p = p * a
-    print(p)
+    while n_input > 0:
+        a_1 = n_input % 10
+        n_input = n_input // 10
+        p_out = p_out * a_1
+    print(p_out)
 if __name__ == "__main__":
     main()
