cf1c16d49a422d6cbf81964e314b16ea7a1804cd
khyathimsit <khyathi17@msitprogram.net>
2018-08-06 19:52:18 +0530

    " -> 10 of 10 passed. pylint: 5.26/10 "

--- cspp1-practice/m7/Functions - Assignment-1/assignment1.py
+++ cspp1-practice/m7/Functions - Assignment-1/assignment1.py
@@ -5,7 +5,7 @@ Created on 06-08-2018
 '''This code is to calculate the credit card balance after one year if a person only
 pays the minimum monthly payment required by the credit card company each month'''
 
-def payingDebt_OffInAYear(balance, annual_InterestRate, monthly_PaymentRate):
+def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
     i = 1
     '''
         The following variables contain values as described below:
@@ -15,8 +15,8 @@ def payingDebt_OffInAYear(balance, annual_InterestRate, monthly_PaymentRate):
     '''
     balance_copy = balance
     while i <= 12:
-        mir = (annual_InterestRate) / 12.0
-        mmp = (monthly_PaymentRate)*(balance_copy)
+        mir = (annualInterestRate) / 12.0
+        mmp = (monthlyPaymentRate)*(balance_copy)
         mub = (balance_copy) - (mmp)
         balance_copy = (mub) + (mir*mub)
         i += 1
@@ -26,7 +26,7 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print(payingDebt_OffInAYear(data[0],data[1],data[2]))
+    print(payingDebtOffInAYear(data[0],data[1],data[2]))
 
 if __name__ == "__main__":
     main()
