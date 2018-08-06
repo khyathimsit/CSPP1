8d556c91d60762e3c3eb5b9a49468822d55113f6
khyathimsit <khyathi17@msitprogram.net>
2018-08-06 19:46:13 +0530

    " -> 0 of 10 passed. pylint: 6.11/10 "

---
+++ cspp1-practice/m7/Functions - Assignment-1/assignment1.py
@@ -0,0 +1,33 @@
+'''
+Author:Khyathi
+Created on 06-08-2018
+'''
+'''This code is to calculate the credit card balance after one year if a person only
+pays the minimum monthly payment required by the credit card company each month'''
+
+def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
+    '''
+        The following variables contain values as described below:
+        # balance - the outstanding balance on the credit card
+        # annualInterestRate - annual interest rate as a decimal
+        # monthlyPaymentRate - minimum monthly payment rate as a decimal
+    '''
+
+    i = 1
+    balance_copy = balance
+    while i <= 12:
+        mir = (annualInterestRate) / 12.0
+        mmp = (monthlyPaymentRate)*(balance_copy)
+        mub = (balance_copy) - (mmp)
+        balance_copy = (mub) + (mir*mub)
+        i += 1
+    return "Remaining balance:" + str(round(balance_copy, 2))
+
+def main():
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    print(payingDebtOffInAYear(data[0],data[1],data[2]))
+
+if __name__ == "__main__":
+    main()
