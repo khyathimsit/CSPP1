b98c5470cd9df0e4aae82d16244319d1bef10328
khyathimsit <khyathi17@msitprogram.net>
2018-08-08 21:43:07 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

---
+++ cspp1-practice/m9/p2/assignment2.py
@@ -0,0 +1,38 @@
+'''
+Author : Khyathi
+'''
+def get_guessed_word(secret_word, letters_guessed):
+    '''
+    secret_word: string, the word the user is guessing
+    letters_guessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secret_word have been guessed so far.
+    '''
+    temp_string = secret_word
+    for each_char in letters_guessed:
+        temp_string = temp_string.replace(each_char, "")
+
+    for each_char in temp_string:
+        if each_char != "_":
+            secret_word = secret_word.replace(each_char, "_")
+    return secret_word
+
+
+def main():
+    '''
+    Main function for current assignment
+    '''
+    user_input = input()
+    if user_input:
+        data = user_input.split()
+        secret_word = data[0]
+    else:
+        data = []
+        secret_word = ""
+    list1 = []
+    for j in range(1, len(data)):
+        list1.append(data[j][0])
+    print(get_guessed_word(secret_word, list1))
+
+if __name__ == "__main__":
+    main()
