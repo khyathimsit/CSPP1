ed84c45f08995809a09607bf9cc2d0d3aa942111
khyathimsit <khyathi17@msitprogram.net>
2018-08-10 18:39:43 +0530

    " -> 9 of 9 passed. pylint: 3.75/10 "

--- cspp1-practice/m11/p1/assignment1.py
+++ cspp1-practice/m11/p1/assignment1.py
@@ -20,9 +20,9 @@ def get_word_score(word, n):
     """
     # TO DO ... <-- Remove this comment when you code this function
     SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+        'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
+        'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
+        's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
 
     sum = 0
     length_word = len(word)
