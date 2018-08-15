f54cedf5435f7ec09fb822a4652422edd47465ec
khyathimsit <khyathi17@msitprogram.net>
2018-08-15 16:32:39 +0530

    " -> 8 of 8 passed. pylint: 9.76/10 "

--- cspp1-practice/m15/codecamppoker2/poker.py
+++ cspp1-practice/m15/codecamppoker2/poker.py
@@ -1,4 +1,10 @@
+'''
+Program for poker game
+'''
 def is_straight(hand):
+    '''
+    Straight Function
+    '''
     stng_values = "--23456789TJQKA"
     hand_values = []
     for i in hand:
@@ -10,12 +16,18 @@ def is_straight(hand):
     return True
 
 def is_flush(hand):
+    '''
+    Flush Function
+    '''
     values_set = set({})
     for i in hand:
         values_set.add(i[1])
     return len(values_set) == 1
 
 def is_four(hand):
+    '''
+    Function for 4 of a kind
+    '''
     # count = 1
     # stng_values = "--23456789TJQKA"
     # hand_values = []
@@ -28,16 +40,19 @@ def is_four(hand):
     # if count==4:
     #     return True
     # return False
-    hand_values = [f for f, s in hand]
+    hand_values = [f_1 for f_1, s in hand]
     set_val = set(hand_values)
     if len(set_val) != 2:
         return False
-    for f in set_val:
-        if hand_values.count(f) == 4:
+    for f_1 in set_val:
+        if hand_values.count(f_1) == 4:
             return True
     return False
 
 def is_three(hand):
+    '''
+    Function for 3 of a kind
+    '''
     # count = 1
     # stng_values = "--23456789TJQKA"
     # hand_values = []
@@ -50,42 +65,54 @@ def is_three(hand):
     # if count==3:
     #     return True
     # return False
-    hand_values = [f for f, s in hand]
+    hand_values = [f_1 for f_1, s in hand]
     set_val = set(hand_values)
     if len(set_val) <= 2:
         return False
-    for f in set_val:
-        if hand_values.count(f) == 3:
+    for f_1 in set_val:
+        if hand_values.count(f_1) == 3:
             return True
     return False
 
 def is_onepair(hand):
-    hand_values = [f for f, s in hand]
+    '''
+    Function for one pair
+    '''
+    hand_values = [f_1 for f_1, s in hand]
     set_val = set(hand_values)
-    twopairs = [f for f in set_val if hand_values.count(f) == 2]
+    twopairs = [f_1 for f_1 in set_val if hand_values.count(f_1) == 2]
     if len(twopairs) != 1:
         return False
     return True
 
 def is_full(hand):
-    hand_values = [f for f, s in hand]
+    '''
+    Full hand function
+    '''
+    hand_values = [f_1 for f_1, s in hand]
     set_val = set(hand_values)
     if len(set_val) != 2:
         return False
-    for f in set_val:
+    for f_1 in set_val:
         if hand_values.count(set_val) == 3:
             return True
     return False
 
 def is_twopair(hand):
-    hand_values = [f for f, s in hand]
+    '''
+    Function for two pair
+    '''
+    hand_values = [f_1 for f_1, s in hand]
     set_val = set(hand_values)
-    twopairs = [f for f in set_val if hand_values.count(f) == 2]
+    twopairs = [f_1 for f_1 in set_val if hand_values.count(f_1) == 2]
     if len(twopairs) != 2:
         return False
     return True
 
 def hand_rank(hand):
+    '''
+    Function for hand rank
+    '''
     if is_straight(hand) and is_flush(hand):
         return 8
     if is_flush(hand):
@@ -105,6 +132,8 @@ def hand_rank(hand):
     return 0
 
 def poker(hands):
+    '''Poker function
+    '''
     return max(hands, key=hand_rank)
 
 if __name__ == "__main__":
