67caa9471cbd9d6dd8863bee809e96739ea6f34b
khyathimsit <khyathi17@msitprogram.net>
2018-08-17 17:49:26 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

---
+++ cspp1-practice/CodeCampDocumentDistance/document_distance.py
@@ -0,0 +1,99 @@
+'''
+    Document Distance - A detailed description is given in the PDF
+'''
+import re
+import math
+import copy
+def word_list(input1, input2):
+    f_1 = ""
+    f_2 = ""
+    
+    f_1 = re.sub('[^ a-z]',' ','',input1.lower())
+    f_2 = re.sub('[^ a-z]',' ','',input2.lower())
+    
+    f_1 = f_1.replace("'", "")
+    f_2 = f_2.replace("'", "")
+    print(f_1, f_2)
+
+    
+    list_1 = []
+    list_2 = []
+    
+    list_1 = f_1.split()
+    list_2 = f_2.split()
+    print(list_1)
+   
+    remove_1 = load_stopwords("stopwords.txt")
+    key_list = remove_1.keys()
+
+    for i in key_list:
+        for j in list_1:
+            if i == j:
+                list_1.remove(j)
+    
+    for i in key_list:
+        for j in list_2:
+            if i == j:
+                list_2.remove(j)   
+    
+    return freq_count(list_1, list_2)
+
+def freq_count(list_1, list_2):
+    dict_1 = {}
+    dict_2 = {}
+    common_dict = {}
+    for k in list_1:
+        if k not in dict_1:
+            dict_1[k] = 1
+        else:
+            dict_1[k] += 1
+    #print(dict_1)
+
+    for k in list_2:
+        if k not in dict_2:
+            dict_2[k] = 1
+        else:
+            dict_2[k] += 1
+    #print(dict_2)
+    for i in dict_1:
+        if i in dict_2:
+            common_dict[i] = [dict_1[i], dict_2[i]]
+
+    
+    
+
+
+def similarity(dict1):
+    '''
+        Compute the document distance as given in the PDF
+    '''
+    num_val = 0
+    a_1 = 0
+    b_1 = 0
+    distance = 0
+    for i in dict1:
+        num_val += dict1[i][0] * dict1[i][1]
+        a_1 +=
+
+
+def load_stopwords(filename):
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
+    stopwords = {}
+    with open(filename, 'r') as filename:
+        for line in filename:
+            stopwords[line.strip()] = 0
+    return stopwords
+
+def main():
+    '''
+        take two inputs and call the similarity function
+    '''
+    input1 = input()
+    input2 = input()
+    #print(similarity(input1, input2))
+    print(word_list(input1, input2))
+
+if __name__ == '__main__':
+    main()
