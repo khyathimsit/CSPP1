9c19f81c339d03aeb0ba7de6f846774146626584
khyathimsit <khyathi17@msitprogram.net>
2018-08-17 18:26:51 +0530

    " -> 1 of 3 passed. pylint: 6.53/10 "

--- cspp1-practice/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/CodeCampDocumentDistance/document_distance.py
@@ -107,7 +107,7 @@ def main():
     input1 = input()
     input2 = input()
     common_dict_1 = word_list(input1, input2)
-    print(word_list(input1, input2))
+    print(similarity(common_dict_1))
 
 if __name__ == '__main__':
     main()
