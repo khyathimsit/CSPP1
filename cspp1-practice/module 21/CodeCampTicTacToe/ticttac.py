09f1592bd0c7e2ec823037a3ede695a1c5f1ef9a
khyathimsit <khyathi17@msitprogram.net>
2018-08-24 14:00:12 +0530

    " -> 12 of 14 passed. pylint: 0.51/10 "

--- cspp1-practice/module 21/CodeCampTicTacToe/ticttac.py
+++ cspp1-practice/module 21/CodeCampTicTacToe/ticttac.py
@@ -65,7 +65,7 @@ def main():
 	    print("invalid input")
 	    cnt += 1
     elif cnt_x > cnt_o + 1 or cnt_o > cnt_x+1:
-	    print("invalid game..")
+	    print("invalid game")
 	    cnt += 1
     turn_x = 'x'
     boolean_x = (horizontal(board,turn_x)
