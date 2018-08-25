b9383f4f88fcdcad7e5fc1e93d8359f77f782c5f
khyathimsit <khyathi17@msitprogram.net>
2018-08-25 10:55:02 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-practice/module22-exam final/assignment2/clean_input.py
+++ cspp1-practice/module22-exam final/assignment2/clean_input.py
@@ -5,10 +5,13 @@ alphabets in both upper and lower case and numbers.
 import re
 def clean_string(string):
    #string_1 = ""
-   string_1 = re.sub('[^A-Za-z0-9]+','', string)
+   string_1 = re.sub('[^A-Za-z0-9]+', '', string)
    return string_1
 
 def main():
+	'''
+	Main Function
+	'''
     string = input()
     print(clean_string(string))
 
